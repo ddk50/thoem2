45385455ffca895d2b3d0066cc25ba41f6f079fe
Kazushi Takahashi <kazushi@rvm.jp>
2018-02-26 14:06:17 +0900

    お問い合わせフォームのコントローラを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'contacts/new'
+
+  get 'contacts/create'
+
   get 'top/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
