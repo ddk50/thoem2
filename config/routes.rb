2d93ce99a729258503976e3034a8ae3d7ccac3c0
Kazushi Takahashi <kazushi@rvm.jp>
2018-02-26 14:23:20 +0900

    コンタクトフォームを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
+# -*- coding: undecided -*-
 Rails.application.routes.draw do
   get 'contacts/new'
 
   get 'contacts/create'
 
   get 'top/index'
+
+  ## お問い合わせフォーム
+  resources "contacts", only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
